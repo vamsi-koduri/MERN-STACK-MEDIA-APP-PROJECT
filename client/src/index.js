a0513718445060cb2d16ea588cdc6ba841a4bb5e
Student <kodurivamsi3@gmail.com>
2023-12-08 09:52:37 +0530

    setup client and server

---
+++ client/src/index.js
@@ -0,0 +1,20 @@
+import React from "react";
+import ReactDom from 'react-dom';
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
+import reducers from './reducers/posts.js'
+
+import App from "./App.js";
+import { formToJSON } from "axios";
+
+
+
+const store = createStore(reducers, compose(applyMiddleware(thunk)))
+
+ReactDom.render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById('root')
+);
